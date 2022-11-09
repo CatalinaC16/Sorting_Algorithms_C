9eef32d9f8deb2cc32d283a300a47fd9b13a2759
CatalinaC16 <114508940+CatalinaC16@users.noreply.github.com>
2022-11-09 22:03:59 +0200

    Update main.c

--- BinarySearch/main.c
+++ BinarySearch/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+
 int BinarySearch(int *v,int searched, int start, int end){
-    if(start > end) return 0;
+    
+    if(start > end) 
+        return 0;
     else{
         int mid = (start+end) / 2;
         if(searched==v[mid])
             return 1;
-        else if(searched < v[mid])
-            return BinarySearch(v,searched,start,mid-1);
-        else return BinarySearch(v,searched,mid+1,end);
+        else 
+            if(searched < v[mid])
+                            return BinarySearch(v,searched,start,mid-1);
+            else return BinarySearch(v,searched,mid+1,end);
 
     }
 }
